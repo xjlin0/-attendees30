8e786fa1891efc0d2f9628dc3aff96343f6ef4d1
Jack Lin <jlin@castlighthealth.com>
2020-08-03 09:21:35 -0700

    starting csv importer

---
+++ attendees/scripts/load_access_csv.py
@@ -0,0 +1,12 @@
+import os
+import csv
+
+
+def run(household_csv_file, people_csv_file, address_csv_file, *extras):
+    print("running load_access_csv.py... with arguments: ")
+    print("here is household_csv_file: ", household_csv_file)
+    print("here is people_csv_file: ", people_csv_file)
+    print("here is address_csv_file: ", address_csv_file)
+    print(extras)
+    # file_ = open(os.path.join(settings.BASE_DIR, 'filename'))
+    pass
