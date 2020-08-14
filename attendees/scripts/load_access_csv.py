e418be7e797cff9eb9706ee56e93c3fc2f448419
Jack Lin <jlin@castlighthealth.com>
2020-08-14 00:39:57 -0700

    family imported

--- attendees/scripts/load_access_csv.py
+++ attendees/scripts/load_access_csv.py
@@ -1,5 +1,6 @@
 import csv
 
+from attendees.persons.models import Family, FamilyAddress
 from attendees.whereabouts.models import Address
 
 
@@ -9,7 +10,11 @@ def import_household_people_address(household_csv, people_csv, address_csv):
     peoples = csv.DictReader(people_csv)
     addresses = csv.DictReader(address_csv)
 
-    import_addresses(addresses)
+    upserted_address_count = import_addresses(addresses)
+    upserted_household_id_count = import_household_ids(households)
+
+    print('Number of address successfully imported/updated: ', upserted_address_count)
+    print('Number of households successfully imported/updated: ', upserted_household_id_count)
 
     try:
         # for household in households:
@@ -34,24 +39,56 @@ def import_addresses(addresses):
         count = 0
         for address in addresses:
             print('Importing: ', address)
-            address_id = int(address['AddressID'])
-            address_values={
-                'street1': address.get('Street'),
-                'city': address.get('City'),
-                'state': address.get('State'),
-                'zip_code': address.get('Zip'),
-                'country': address.get('Country'),
-                'fields': {'access_address_id': address_id}
-            }
-            Address.objects.update_or_create(
-                fields__access_address_id=address_id,
-                defaults=address_values
-            )
-            count += 1
-        print('Number of address successfully imported/updated: ', count)
+            address_id = int('0' + address.get('AddressID', '0'))
+            if address_id > 0:
+                address_values = {
+                    'street1': address.get('Street'),
+                    'city': address.get('City'),
+                    'state': address.get('State'),
+                    'zip_code': address.get('Zip'),
+                    'country': address.get('Country'),
+                    'fields': {'access_address_id': address_id}
+                }
+                Address.objects.update_or_create(
+                    fields__access_address_id=address_id,
+                    defaults=address_values
+                )
+                count += 1
+        return count
 
     except Exception as e:
         print('Cannot proceed import_addresses, reason: ', e)
+    pass
+
+
+def import_household_ids(households):
+    try:
+        count = 0
+        for household in households:
+            print('Importing: ', household)
+            household_id = int('0' + household.get('HouseholdID', '0'))
+            address_id = int('0' + household.get('AddressID', '0'))
+            if household_id > 0:
+                household_values = {
+                    'infos': {'access_household_id': household_id}
+                }
+
+                family, created = Family.objects.update_or_create(
+                    infos__access_household_id=household_id,
+                    defaults=household_values
+                )
+
+                if address_id > 0:
+                    FamilyAddress.objects.update_or_create(
+                        family=family,
+                        address=Address.objects.get(fields__access_address_id=address_id)
+                    )
+
+                count += 1
+        return count
+
+    except Exception as e:
+        print('Cannot proceed import_households, reason: ', e)
     pass
 
 
