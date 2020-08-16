34e8e46158bcadcb4f4d5d70ad64432c78501ee1
Jack Lin <jlin@castlighthealth.com>
2020-08-15 23:10:25 -0700

    attendee id generated without duplication, but cannot update new values

--- attendees/scripts/load_access_csv.py
+++ attendees/scripts/load_access_csv.py
@@ -1,6 +1,6 @@
-import csv, uuid
-
-from attendees.persons.models import Family, FamilyAddress, Utility
+import csv
+from datetime import datetime
+from attendees.persons.models import Family, FamilyAddress, Utility, GenderEnum, Attendee
 from attendees.whereabouts.models import Address, Division
 
 
@@ -11,21 +11,12 @@ def import_household_people_address(household_csv, people_csv, address_csv, divi
     addresses = csv.DictReader(address_csv)
 
     try:
-        # for household in households:
-        #     pass
-        #     print(household)
-        # for people in peoples:
-        #     pass
-        #     print(people)
-        # for address in addresses:
-        #     pass
-        #     print(address)
         upserted_address_count = import_addresses(addresses)
         upserted_household_id_count = import_household_ids(households, division1_slug, division2_slug)
-
+        upserted_attendee_count = import_attendee_id(peoples)
         print('Number of address successfully imported/updated: ', upserted_address_count)
         print('Number of households successfully imported/updated: ', upserted_household_id_count)
-
+        print('Number of people successfully imported/updated: ', upserted_attendee_count)
     except Exception as e:
         print('Cannot proceed import_household_people_address, reason: ', e)
 
@@ -121,6 +112,66 @@ def import_household_ids(households, division1_slug, division2_slug):
     pass
 
 
+def import_attendee_id(peoples):
+    gender_converter = {
+        'F': GenderEnum.FEMALE,
+        'M': GenderEnum.MALE,
+    }
+    try:
+        count = 0
+        for people in peoples:
+            print('Importing/updating: ', people)
+            first_name = Utility.presence(people.get('FirstName'))
+            last_name = Utility.presence(people.get('LastName'))
+            birth_date = Utility.presence(people.get('BirthDate'))
+            name2 = Utility.presence(people.get('ChineseName'))
+            household_id = Utility.presence(people.get('HouseholdID'))
+            household_role = Utility.presence(people.get('HouseholdRole'))
+
+            if household_id and (first_name or last_name):
+
+                attendee_values = {
+                    'first_name': first_name,
+                    'last_name': last_name,
+                    'first_name2': None,
+                    'last_name2': None,
+                    'gender': gender_converter.get(Utility.presence(people.get('Sex', '').upper()), GenderEnum.UNSPECIFIED).value,
+                    'infos': {
+                        'access_people_household_id': household_id,
+                        'access_people_values': people,
+                    }
+                }
+
+                if birth_date:
+                    try:
+                        attendee_values['actual_birthday'] = datetime.strptime(birth_date, '%m/%d/%Y').strftime('%Y-%m-%d')
+                    except ValueError:
+                        print('import_attendee_id error on BirthDate, people: ', people)
+
+                if name2:  # assume longest last name is 2 characters
+                    break_position = -2 if len(name2) > 2 else -1
+                    attendee_values['first_name2'] = name2[break_position:]
+                    attendee_values['last_name2'] = name2[:break_position]
+
+                query_values = {
+                    'first_name': attendee_values['first_name'],
+                    'last_name': attendee_values['last_name'],
+                    'first_name2': attendee_values['first_name2'],
+                    'last_name2': attendee_values['last_name2'],
+                    'infos__access_people_household_id': household_id,
+                }
+
+                Attendee.objects.update_or_create(
+                    **{k: v for (k, v) in query_values.items() if v is not None},
+                    defaults={k: v for (k, v) in attendee_values.items() if v is not None}
+                )
+                count += 1
+        return count
+
+    except Exception as e:
+        print('Cannot proceed import_addresses, reason: ', e)
+    pass
+
 def check_all_headers():
     #households_headers = ['HouseholdID', 'HousholdLN', 'HousholdFN', 'SpouseFN', 'AddressID', 'HouseholdPhone', 'HouseholdFax', 'AttendenceCount', 'FlyerMailing', 'CardMailing', 'UpdateDir', 'PrintDir', 'LastUpdate', 'HouseholdNote', 'FirstDate', '海沃之友', 'Congregation']
     #peoples_headers = ['LastName', 'FirstName', 'NickName', 'ChineseName', 'Photo', 'Sex', 'Active', 'HouseholdID', 'HouseholdRole', 'E-mail', 'WorkPhone', 'WorkExtension', 'CellPhone', 'BirthDate', 'Skills', 'FirstDate', 'BapDate', 'BapLocation', 'Member', 'MemberDate', 'Fellowship', 'Group', 'LastContacted', 'AssignmentID', 'LastUpdated', 'PeopleNote', 'Christian']
@@ -147,7 +198,7 @@ def run(household_csv_file, people_csv_file, address_csv_file, division1_slug, d
     print(division1_slug)
     print(division2_slug)
     print(extras)
-    print("divisions required for importing, running commands: docker-compose -f local.yml run django python manage.py runscript load_access_csv --script-args path/tp/household.csv path/to/people.csv path/to/address.csv cfcc-chinese-ministry cfcc-crossing-ministry")
+    print("divisions required for importing, running commands: docker-compose -f local.yml run django python manage.py runscript load_access_csv --script-args path/tp/household.csv path/to/people.csv path/to/address.csv division1_slug division2_slug")
 
     if household_csv_file and people_csv_file and address_csv_file and division1_slug and division2_slug:
         with open(household_csv_file, mode='r', encoding='utf-8-sig') as household_csv, open(people_csv_file, mode='r', encoding='utf-8-sig') as people_csv, open(address_csv_file, mode='r', encoding='utf-8-sig') as address_csv:
