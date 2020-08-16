978f517e9d29f4e53f0fe238e4954d87b29f2358
Jack Lin <jlin@castlighthealth.com>
2020-08-15 20:02:26 -0700

    family impport divisions now

--- attendees/scripts/load_access_csv.py
+++ attendees/scripts/load_access_csv.py
@@ -1,20 +1,14 @@
-import csv
+import csv, uuid
 
 from attendees.persons.models import Family, FamilyAddress, Utility
-from attendees.whereabouts.models import Address
+from attendees.whereabouts.models import Address, Division
 
 
-def import_household_people_address(household_csv, people_csv, address_csv):
+def import_household_people_address(household_csv, people_csv, address_csv, division1_slug, division2_slug):
     print("running import_household_people_address ...")
     households = csv.DictReader(household_csv)
     peoples = csv.DictReader(people_csv)
     addresses = csv.DictReader(address_csv)
-
-    upserted_address_count = import_addresses(addresses)
-    upserted_household_id_count = import_household_ids(households)
-
-    print('Number of address successfully imported/updated: ', upserted_address_count)
-    print('Number of households successfully imported/updated: ', upserted_household_id_count)
 
     try:
         # for household in households:
@@ -26,7 +20,11 @@ def import_household_people_address(household_csv, people_csv, address_csv):
         # for address in addresses:
         #     pass
         #     print(address)
-        pass
+        upserted_address_count = import_addresses(addresses)
+        upserted_household_id_count = import_household_ids(households, division1_slug, division2_slug)
+
+        print('Number of address successfully imported/updated: ', upserted_address_count)
+        print('Number of households successfully imported/updated: ', upserted_household_id_count)
 
     except Exception as e:
         print('Cannot proceed import_household_people_address, reason: ', e)
@@ -65,7 +63,13 @@ def import_addresses(addresses):
     pass
 
 
-def import_household_ids(households):
+def import_household_ids(households, division1_slug, division2_slug):
+    division1 = Division.objects.get(slug=division1_slug)
+    division2 = Division.objects.get(slug=division2_slug)
+    division_converter = {
+        'CH': division1,
+        'EN': division2,
+    }
     try:
         count = 0
         for household in households:
@@ -73,16 +77,22 @@ def import_household_ids(households):
             household_id = Utility.presence(household.get('HouseholdID'))
             address_id = Utility.presence(household.get('AddressID'))
             display_name = (household.get('HousholdFN', '') + ' ' + household.get('SpouseFN', '')).strip()
+            congregation = Utility.presence(household.get('Congregation'))
+
             if household_id:
                 household_values = {
                     'display_name': display_name,
                     'infos': {
                         'access_household_id': household_id,
                         'access_household_values': household,
-                        'congregation': Utility.presence(household.get('Congregation')),
                         'last_update': Utility.presence(household.get('LastUpdate')),
                     }
                 }
+
+                if congregation:
+                    division = division_converter.get(congregation)
+                    if division:
+                        household_values['division'] = division
 
                 family, family_created = Family.objects.update_or_create(
                     infos__access_household_id=household_id,
@@ -118,13 +128,15 @@ def check_all_headers():
     pass
 
 
-def run(household_csv_file, people_csv_file, address_csv_file, *extras):
+def run(household_csv_file, people_csv_file, address_csv_file, division1_slug, division2_slug, *extras):
     """
     An importer to import old MS Access db data, if same records founds in Attendees db, it will update.
     :param household_csv_file: a comma separated file of household with headers, from MS Access
     :param people_csv_file: a comma separated file of household with headers, from MS Access
     :param address_csv_file: a comma separated file of household with headers, from MS Access
-    :param extras: other arguments
+    :param division1_slug: slug of division 1
+    :param division1_slug: slug of division 2
+    :param extras: optional other arguments
     :return: None, but write to Attendees db (create or update)
     """
 
@@ -132,8 +144,11 @@ def run(household_csv_file, people_csv_file, address_csv_file, *extras):
     print("here is household_csv_file: ", household_csv_file)
     print("here is people_csv_file: ", people_csv_file)
     print("here is address_csv_file: ", address_csv_file)
+    print(division1_slug)
+    print(division2_slug)
     print(extras)
+    print("divisions required for importing, running commands: docker-compose -f local.yml run django python manage.py runscript load_access_csv --script-args path/tp/household.csv path/to/people.csv path/to/address.csv cfcc-chinese-ministry cfcc-crossing-ministry")
 
-    if household_csv_file and people_csv_file and address_csv_file:
+    if household_csv_file and people_csv_file and address_csv_file and division1_slug and division2_slug:
         with open(household_csv_file, mode='r', encoding='utf-8-sig') as household_csv, open(people_csv_file, mode='r', encoding='utf-8-sig') as people_csv, open(address_csv_file, mode='r', encoding='utf-8-sig') as address_csv:
-            import_household_people_address(household_csv, people_csv, address_csv)
+            import_household_people_address(household_csv, people_csv, address_csv, division1_slug, division2_slug)
