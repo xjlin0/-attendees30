ee592722cae9a740042c6d8fd318d1f74a6ed983
Jack Lin <jlin@castlighthealth.com>
2020-11-20 08:24:53 -0800

    starting port server side test pages

--- attendees/persons/urls.py
+++ attendees/persons/urls.py
@@ -6,6 +6,7 @@ from attendees.persons.views import (
     api_data_attendings_viewset,
     api_assembly_meet_attendees_viewset,
     datagrid_assembly_all_attendings_list_view,
+    datagrid_assembly_test_attendings_list_view,
     datagrid_assembly_data_attendings_list_view,
     api_user_meet_attendings_viewset,
     api_family_organization_attendings_viewset,
@@ -14,11 +15,21 @@ from attendees.persons.views import (
 app_name = "persons"
 
 router = routers.DefaultRouter()
+# router.register(
+#     'api/odata_attendings',
+#     api_odata_attendings_viewset,
+#     basename='attending',
+# )
 router.register(
     'api/(?P<division_slug>.+)/(?P<assembly_slug>.+)/assembly_meet_attendings',
     api_assembly_meet_attendings_viewset,
     basename='attending',
 )
+# router.register(
+#     'api/(?P<division_slug>.+)/(?P<assembly_slug>.+)/test_attendings',
+#     api_data_attendings_viewset,
+#     basename='attending',
+# )
 router.register(
     'api/(?P<division_slug>.+)/(?P<assembly_slug>.+)/data_attendings',
     api_data_attendings_viewset,
@@ -49,6 +60,12 @@ urlpatterns = [
         "<slug:division_slug>/<slug:assembly_slug>/datagrid_assembly_all_attendings/",
         view=datagrid_assembly_all_attendings_list_view,
         name="datagrid_assembly_all_attendings",
+    ),
+
+    path(
+        "<slug:division_slug>/<slug:assembly_slug>/datagrid_assembly_test_attendings/",
+        view=datagrid_assembly_test_attendings_list_view,
+        name="datagrid_assembly_test_attendings",
     ),
 
     path(
