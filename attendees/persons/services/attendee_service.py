34c2396f09c884e91b393cdc3b13fd9df8e208b6
Jack Lin <jlin@castlighthealth.com>
2020-11-27 07:25:11 -0800

    skip order by meet for now since it needs both annotation and orderby, causing performance impact on query

--- attendees/persons/services/attendee_service.py
+++ attendees/persons/services/attendee_service.py
@@ -38,35 +38,62 @@ class AttendeeService:
 
         final_query = init_query.add(AttendeeService.filter_parser(filters_list), Q.AND)
 
+        annotation_conditions = AttendeeService.annotation_parser(orderby_string)
+
         return Attendee.objects.select_related().prefetch_related().annotate(
-                joined_meets=ArrayAgg('attendings__meets__slug', distinct=True, order='slug'),
-                # joined_roaster=Count('attendings__meets__slug', filter=Q(attendings__meets__slug='d7c8Fd_cfcc_congregation_roaster'), distinct=True),
-                # joined_roaster=Count(Case(When(attendings__meets__slug='d7c8Fd_cfcc_congregation_roaster', then=1), output_field=IntegerField())),
+                **annotation_conditions
                 ).filter(final_query).order_by(*orderby_list)
 
     @staticmethod
     def orderby_parser(orderby_string):
-        orderby_list = []
-        # how to sort joined_meets like [{"selector":"d7c8Fd_cfcc_congregation_roaster","desc":false}] ?
+        """
+        Not supporting order by meet yet: it needs both annotation and order_by
+        :param orderby_string: JSON fetched from search params, will convert attendee.division to attendee__division
+        :return: a List of string for order_by()
+        """
+        orderby_list = []  # sort joined_meets is [{"selector":"<<dataField value in DataGrid>>","desc":false}]
         for orderby_dict in json.loads(orderby_string):
             direction = '-' if orderby_dict.get('desc', False) else ''
-            field = orderby_dict.get('selector', 'id').replace('.', '__')  # convert attendee.division to attendee__division
+            field = orderby_dict.get('selector', 'id').replace('.', '__')
             orderby_list.append(direction + field)
         return orderby_list
 
     @staticmethod
+    def annotation_parser(orderby_string):
+        """
+        Not supporting order by meet yet: it needs both annotation and order_by,
+        (annotation create a new column, such as counting by conditions, for order by later)
+        :param orderby_string: orderby_string: JSON fetched from search params
+        :return: a dictionary of annotations
+        """
+        return {
+            'joined_meets': ArrayAgg('attendings__meets__slug', distinct=True)  # , order='slug')
+            # 'roaster_count': Count(Case(When(attendings__meets__slug='d7c8Fd_cfcc_congregation_roaster', then=1)))
+            # 'joined_roaster': Count('attendings__meets__slug', filter=Q(attendings__meets__slug='d7c8Fd_cfcc_congregation_roaster'), distinct=True),
+            # 'joined_roaster': Count(Case(When(attendings__meets__slug='d7c8Fd_cfcc_congregation_roaster', then=1), output_field=IntegerField())),
+        }
+
+    @staticmethod
     def filter_parser(filters_list):
+        """
+        A recursive method return Q function based on multi-level filter conditions
+        :param filters_list: a string of multi-level list of filter conditions
+        :return: Q function, could be an empty Q()
+        """
+        and_string = Q.AND.lower()
+        or_string = Q.OR.lower()
+
         if filters_list:
-            if 'and' in filters_list and 'or' in filters_list:
-                raise Exception('cannot process both or + and at the same level! please wrap them in separated lists')
-            elif filters_list[1] == 'and':
-                and_list = [element for element in filters_list if element != 'and']
+            if and_string in filters_list and or_string in filters_list:
+                raise Exception("Can't process both 'or'/'and' at the same level! please wrap them in separated lists.")
+            elif filters_list[1] == and_string:
+                and_list = [element for element in filters_list if element != and_string]
                 and_query = AttendeeService.filter_parser(and_list[0])
                 for and_element in and_list[1:]:
                     and_query.add(AttendeeService.filter_parser(and_element), Q.AND)
                 return and_query
-            elif filters_list[1] == 'or':
-                or_list = [element for element in filters_list if element != 'or']
+            elif filters_list[1] == or_string:
+                or_list = [element for element in filters_list if element != or_string]
                 or_query = AttendeeService.filter_parser(or_list[0])
                 for or_element in or_list[1:]:
                     or_query.add(AttendeeService.filter_parser(or_element), Q.OR)
--- attendees/static/js/persons/datagrid_assembly_data_attendees.js
+++ attendees/static/js/persons/datagrid_assembly_data_attendees.js
@@ -90,7 +90,7 @@ Attendees.dataAttendees = {
     },
     pager: {
         showPageSizeSelector: true,
-        allowedPageSizes: [15, 150, 1500]
+        allowedPageSizes: [10, 20, 5000]
     },
     columns: [
       {
@@ -105,7 +105,7 @@ Attendees.dataAttendees = {
       },
       {
         caption: "member",
-        dataField: "hi5",
+        dataField: "",
         dataType: "string",
         calculateCellValue: (rowData)=>{
           meet_slug='d7c8Fd_cfcc_congregation_roaster'
