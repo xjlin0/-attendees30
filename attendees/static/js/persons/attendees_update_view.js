329282f11410535a4103ce9974958e993df3045e
Jack Lin <jlin@castlighthealth.com>
2020-12-12 15:25:03 -0800

    switching mode of buttons works

--- attendees/static/js/persons/attendees_update_view.js
+++ attendees/static/js/persons/attendees_update_view.js
@@ -1,27 +1,30 @@
 Attendees.formsetUpdate = {
   init: () => {
     console.log("/static/js/persons/attendees_update_view.js");
-
-    Attendees.formsetUpdate.expandAccordionAndEnableEditButton();
-    Attendees.formsetUpdate.switchEditModeUponButtons();
-  },
-
-  editButtonSelectors: 'input.cancel-edit-reset,input.submit-formset,input.reset-editing',
-
-  expandAccordionAndEnableEditButton: ()=>{
-    const editButton = document.querySelector('button.enable-edit');
-
     document.querySelector('a[data-toggle="collapse"]').click(); // open the first Accordion.
-
-    editButton.disabled = false;
-    editButton.addEventListener('click', ()=>{
-      editButton.classList.add('d-none');
-      document.querySelectorAll(Attendees.formsetUpdate.editButtonSelectors).forEach(ele => ele.classList.remove('d-none'));
-    }, false);
+    Attendees.formsetUpdate.enableEditButton();
   },
 
-  switchEditModeUponButtons: () => {
-    const editButton = document.querySelector('button.enable-edit');
+  enableEditButton: ()=>{
+    const enableEditButton = document.querySelector('button.enable-edit');
+    const editButtons = document.querySelectorAll('input.cancel-edit-reset,input.submit-formset,input.reset-editing');
+    enableEditButton.disabled = false;
+
+    document.querySelector('form.attendees-formset').addEventListener('click', ()=>{
+      switch(true) {
+        case /enable-edit/.test(event.target.className):
+          enableEditButton.classList.add('d-none');
+          editButtons.forEach(ele => ele.classList.remove('d-none'));
+          break;
+
+        case /cancel-edit-reset/.test(event.target.className):
+          enableEditButton.classList.remove('d-none');
+          editButtons.forEach(ele => ele.classList.add('d-none'));
+          break;
+
+        default:
+      }
+    }, false);
   },
 }
 
