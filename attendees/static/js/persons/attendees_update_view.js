81bf5ec6eebf196e31fad7d5fef450531c1db276
Jack Lin <jlin@castlighthealth.com>
2020-12-12 14:33:33 -0800

    make edit button shows upon enable editing

--- attendees/static/js/persons/attendees_update_view.js
+++ attendees/static/js/persons/attendees_update_view.js
@@ -3,11 +3,25 @@ Attendees.formsetUpdate = {
     console.log("/static/js/persons/attendees_update_view.js");
 
     Attendees.formsetUpdate.expandAccordionAndEnableEditButton();
+    Attendees.formsetUpdate.switchEditModeUponButtons();
   },
 
+  editButtonSelectors: 'input.cancel-edit-reset,input.submit-formset,input.reset-editing',
+
   expandAccordionAndEnableEditButton: ()=>{
-    document.querySelector('a[data-toggle="collapse"]').click(); // open the first
-    document.querySelector('button.enable-edit').disabled = false;
+    const editButton = document.querySelector('button.enable-edit');
+
+    document.querySelector('a[data-toggle="collapse"]').click(); // open the first Accordion.
+
+    editButton.disabled = false;
+    editButton.addEventListener('click', ()=>{
+      editButton.classList.add('d-none');
+      document.querySelectorAll(Attendees.formsetUpdate.editButtonSelectors).forEach(ele => ele.classList.remove('d-none'));
+    }, false);
+  },
+
+  switchEditModeUponButtons: () => {
+    const editButton = document.querySelector('button.enable-edit');
   },
 }
 
