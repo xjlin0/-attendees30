59a8e25ce0cf6646e03626365504e86a81484b86
Jack Lin <jlin@castlighthealth.com>
2021-02-15 12:35:04 -0800

    revert back to use original django-address by class based on banagale's suggestion

--- attendees/whereabouts/migrations/0003_contact.py
+++ attendees/whereabouts/migrations/0003_contact.py
@@ -20,7 +20,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Contact',
             fields=[
-                ('id', model_utils.fields.UUIDField(default=uuid4, editable=False, primary_key=True, serialize=False)),
+                # ('id', model_utils.fields.UUIDField(default=uuid4, editable=False, primary_key=True, serialize=False)),
+                ('address_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='address.Address')),
                 ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
                 ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
                 ('is_removed', models.BooleanField(default=False)),
@@ -29,9 +30,8 @@ class Migration(migrations.Migration):
                 # ('email2', models.EmailField(blank=True, max_length=254, null=True)),
                 # ('phone1', models.CharField(blank=True, db_index=True, max_length=16, null=True)),
                 # ('phone2', models.CharField(blank=True, max_length=16, null=True)),
-                ('address_type', models.CharField(max_length=20, default='street', blank=True, null=True, help_text='mailing, remote or street address')),
-                ('address', AddressField(blank=True, null=True, on_delete=models.SET_NULL)),
                 ('address_extra', models.CharField(max_length=50, blank=True, null=True, help_text='i.e. Apartment number')),
+                ('address_type', models.CharField(max_length=20, default='street', blank=True, null=True, help_text='mailing, remote or street address')),
                 # ('city', models.CharField(max_length=50, blank=True, null=True)),
                 # ('state', models.CharField(default='CA', max_length=10, blank=True, null=True)),
                 # ('zip_code', models.CharField(max_length=10, null=True, blank=True)),
@@ -42,10 +42,14 @@ class Migration(migrations.Migration):
             options={
                 'db_table': 'whereabouts_contacts',
                 'verbose_name_plural': 'Contacts',
-                'ordering': ['created'],
+                'ordering': ('locality', 'route', 'street_number', 'address_extra'),
             },
             bases=(models.Model, attendees.persons.models.utility.Utility),
         ),
+        # migrations.RemoveField(
+        #     model_name='contact',
+        #     name='id',
+        # ),
         migrations.AddIndex(
             model_name='contact',
             index=GinIndex(fields=['fields'], name='contact_fields_gin'),
