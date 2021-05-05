2669d48a7a4fcdcefe747752c0936eeeb76d8c68
Jack Lin <jlin@castlighthealth.com>
2021-05-05 10:00:38 -0700

    add campus association to Locate

--- attendees/whereabouts/migrations/0005_campus.py
+++ attendees/whereabouts/migrations/0005_campus.py
@@ -4,6 +4,7 @@ import attendees.persons.models.utility
 from django.db import migrations, models
 import django.db.models.deletion
 import django.utils.timezone
+from django.contrib.postgres.fields.jsonb import JSONField
 import model_utils.fields
 
 
@@ -21,14 +22,19 @@ class Migration(migrations.Migration):
                 ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
                 ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
                 ('is_removed', models.BooleanField(default=False)),
-                # ('contact', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='whereabouts.Contact')),
                 ('display_name', models.CharField(db_index=True, max_length=50)),
                 ('slug', models.SlugField(max_length=50, unique=True)),
+                ('infos', JSONField(blank=True, default=dict, help_text='Example: {"hostname": "where the app deployed"}. Please keep {} here even no data', null=True)),
+
             ],
             options={
                 'db_table': 'whereabouts_campus',
                 'verbose_name_plural': 'Campuses',
             },
             bases=(models.Model, attendees.persons.models.utility.Utility),
+        ),
+        migrations.AddIndex(
+            model_name='campus',
+            index=django.contrib.postgres.indexes.GinIndex(fields=['infos'], name='campus_infos_gin'),
         ),
     ]
