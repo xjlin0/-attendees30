4672b56425da100c6f12dc8d109e4ad16deb91a9
Jack Lin <jlin@castlighthealth.com>
2021-05-05 10:12:20 -0700

    add Locate associate to property

--- attendees/persons/models/family.py
+++ attendees/persons/models/family.py
@@ -9,7 +9,6 @@ class Family(UUIDModel, TimeStampedModel, SoftDeletableModel):
     locates = GenericRelation('whereabouts.Locate')
     division = models.ForeignKey('whereabouts.Division', default=0, null=False, blank=False, on_delete=models.SET(0))
     attendees = models.ManyToManyField('persons.Attendee', through='FamilyAttendee', related_name='attendees')
-    # contacts = models.ManyToManyField('whereabouts.Place', through='FamilyContact')
     display_name = models.CharField(max_length=50, blank=True, null=True)
     display_order = models.SmallIntegerField(default=0, blank=False, null=False, db_index=True)
     infos = JSONField(null=True, blank=True, default=dict, help_text='Example: {"2010id": "3"}. Please keep {} here even no data')
