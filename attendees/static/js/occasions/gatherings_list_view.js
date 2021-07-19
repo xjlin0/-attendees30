1c0c47c795fc44e77750a76f11cf459c92bff8eb
Jack Lin <jlin@castlighthealth.com>
2021-07-19 09:44:08 -0700

    grouped drop down of meet works but datetime selector does not

--- attendees/static/js/occasions/gatherings_list_view.js
+++ attendees/static/js/occasions/gatherings_list_view.js
@@ -1,5 +1,6 @@
 Attendees.gatherings = {
   filtersForm: null,
+  meetTagbox: null,
   init: () => {
     console.log('static/js/occasions/gatherings_list_view.js');
     Attendees.gatherings.initFiltersForm();
@@ -9,7 +10,7 @@ Attendees.gatherings = {
   initListeners: () => {},
   initFiltersForm: () => {
     console.log('11 initFiltersForm');
-    Attendees.gatherings.filtersForm = $('div.gathering-filters').dxForm(Attendees.gatherings.filterFormConfigs).dxForm('instance');
+    Attendees.gatherings.filtersForm = $('div.filters-dxform').dxForm(Attendees.gatherings.filterFormConfigs).dxForm('instance');
   },
   filterFormConfigs: {
     dataSource: null,
@@ -19,6 +20,7 @@ Attendees.gatherings = {
       {
         dataField: 'filter_from',
         colSpan: 3,
+        // cssClass: 'filter_from',
         validationRules: [{type: 'required'}],
         label: {
           location: 'top',
@@ -33,6 +35,7 @@ Attendees.gatherings = {
       {
         dataField: 'filter_till',
         colSpan: 3,
+        // cssClass: 'filter_till',
         validationRules: [{type: 'required'}],
         label: {
           location: 'top',
@@ -57,21 +60,54 @@ Attendees.gatherings = {
         editorOptions: {
           valueExpr: 'id',
           displayExpr: 'display_name',
-          // grouped: true,
+          showClearButton: true,
+          grouped: true,
           dataSource: new DevExpress.data.DataSource({
             store: new DevExpress.data.CustomStore({
               key: 'id',
-              load: () => $.getJSON($('div.gathering-filters').data('meets-endpoint')),
-            }),
+              load: () => {
+                const d = new $.Deferred();
+                $.get($('div.filters-dxform').data('meets-endpoint'), {
+                  start: $('input[name="filter_from"]').val(),
+                  finish: $('input[name="filter_till"]').val(),
+                })
+                  .done((result) => {
+                    const answer={};
+                    if (result.data[0] && result.data[0].assembly_name){
+                      result.data.forEach(meet=>{
+                        if (meet.assembly_name){
+                          answer[meet.assembly_name] = answer[meet.assembly_name] || {key: meet.assembly_name, items:[]};
+                          answer[meet.assembly_name].items.push(meet);
+                        }
+                      })
+                    }
+                    d.resolve(Object.values(answer));
+                  });
+                return d.promise();
+              },
+            }),  // specify group didn't work, so do it manually :(
             key: 'id',
-            group: 'assembly_name',
           }),
         },
+
+        // editorOptions: {
+        //   valueExpr: 'id',
+        //   displayExpr: 'display_name',
+        //   // grouped: true,
+        //   dataSource: new DevExpress.data.DataSource({
+        //     store: new DevExpress.data.CustomStore({
+        //       key: 'id',
+        //       load: () => $.getJSON($('div.filters-dxform').data('meets-endpoint')),
+        //     }),
+        //     key: 'id',
+        //     group: 'assembly_name',
+        //   }),
+        // },
       },
     ],
   },
   initFilteredGatheringsDatagrid: () => {},
-}
+};
 
 $(document).ready(() => {
   Attendees.gatherings.init();
