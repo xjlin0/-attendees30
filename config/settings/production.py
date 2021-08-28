d03291cbe7d612f5d849c293104726db84a388fb
Jack Lin <jlin@castlighthealth.com>
2021-08-28 08:15:14 -0700

    prepare for new model message

--- config/settings/production.py
+++ config/settings/production.py
@@ -101,7 +101,7 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="Attendees <noreply@chineseforchristchurch.org>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="Attendees <xjlin0@gmail.com>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
