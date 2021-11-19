747b978f9d6c802d9b900beb19084c476ec5c217
Jack Lin <jlin@castlighthealth.com>
2021-11-18 17:01:20 -0800

    order to escape default 20 limit

--- attendees/occasions/views/api/user_assembly_meets.py
+++ attendees/occasions/views/api/user_assembly_meets.py
@@ -1,12 +1,13 @@
 import time
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed
-from django.db.models import F
+from django.db.models import F, Case, When
 from attendees.occasions.models import Meet
 from attendees.occasions.serializers.meet import MeetSerializer
+from attendees.persons.models import Attendee, AttendingMeet
 
 
 class ApiUserAssemblyMeetsViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
@@ -18,6 +19,7 @@ class ApiUserAssemblyMeetsViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
     def get_queryset(self):
         current_user = self.request.user
         current_user_organization = current_user.organization
+        target_attendee = get_object_or_404(Attendee, pk=self.request.META.get('HTTP_X_TARGET_ATTENDEE_ID'))
         #  Todo: this endpoint is used by datagrid_attendee_update_view page (with params). Do check if the editor and the editing target relations and permissions
         if current_user_organization:
             filters = {'assembly__division__organization': current_user_organization}
@@ -26,6 +28,8 @@ class ApiUserAssemblyMeetsViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
                 filters['assembly__in'] = assemblies
             return Meet.objects.filter(**filters).annotate(
                 assembly_name=F('assembly__display_name'),
+            ).order_by(
+                Case(When(id__in=AttendingMeet.objects.filter(attending__attendee=target_attendee).values_list('meet').distinct(), then=0), default=1),
             )
 
         else:
